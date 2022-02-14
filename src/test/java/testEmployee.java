de4ad3352f2ef594f9f26811d7b464a7f661c4c9
Marcus Davidsson <marcus.davidsson@iths.se>
2022-02-14 13:33:31 +0100

    RED (kompileringsfel)

--- src/test/java/testEmployee.java
+++ src/test/java/testEmployee.java
@@ -1,8 +1,12 @@
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class testEmployee {
 
-    Employee testObject;
+    Employee testObject1;
+    Employee testObject2;
+    Employee testObject3;
 
         @BeforeAll
         public static void beforeAll(){
@@ -20,7 +24,9 @@ public class testEmployee {
         public void beforeEach(){
             System.out.println("Test start");
 
-            testObject = new Employee("Marcus", "Davidsson", 33, 10.000);
+            testObject1 = new Employee("Marcus", "Davidsson", 33, 10.000);
+            testObject2 = new Employee("Jacob", "Nilsson", 23, 10.000);
+            testObject3 = new Employee("Anki", "Phan", 24, 10.000);
         }
 
 
@@ -30,8 +36,27 @@ public class testEmployee {
             int expected = 1001;
 
 
-            int actual = testObject.getUniqueID();
+            int actual = testObject1.getUniqueID();
 
+            Assertions.assertEquals(expected, actual);
+
+        }
+
+        @ParameterizedTest
+        public void checkGetUniqueID2test(){
+
+            int expected1 = 1001;
+            int expected2 = 1002;
+            int expected3 = 1003;
+
+
+            int actual1 = testObject1.getUniqueID();
+            int actual2 = testObject2.getUniqueID();
+            int actual3 = testObject3.getUniqueID();
+
+            Assertions.assertEquals(expected1, actual1);
+            Assertions.assertEquals(expected2, actual2);
+            Assertions.assertEquals(expected3, actual3);
 
         }
 }
